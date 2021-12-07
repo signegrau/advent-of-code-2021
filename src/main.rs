ad969668a6004a06dfdcccf1ec2f806095d161b9
Signe Rhea Grau Kristensen <signe@signe.dev>
2021-12-07 23:08:52 +0100

    Day 2

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,6 @@ fn parse_file(path: &str) -> Vec<i32> {
 fn solve_day1() {
     let count_fn = |(prev, count), n: &i32| (*n, if n > &prev { count + 1 } else { count });
     let lines = parse_file("inputs/day1.txt");
-    
     // Puzzle 1
     let count1: i32 = lines.iter().skip(1).fold((lines[0], 0), count_fn).1;
 
@@ -26,7 +25,46 @@ fn solve_day1() {
     println!("2nd puzzle: {}", count2);
 }
 
+fn solve_day2() {
+    let contents = fs::read_to_string("inputs/day2.txt").expect("Could not read file");
+
+    let movements: Vec<_> = contents.split('\n').map(|l| {
+        let arr: Vec<_> = l.split(' ').collect();
+        match arr[..] {
+            [c, d] => (c, d.parse::<i32>().unwrap()),
+            _ => ("", 0)
+        }
+    }).collect();
+
+    let puzzle1 = match movements.iter().fold((0, 0), |(z, y), movement| match movement {
+        ("forward", d) => (z + d, y),
+        ("down", d) => (z, y + d),
+        ("up", d) => (z, y - d),
+        _ => (z, y),
+    }) {
+        (z, y) => z * y,
+    };
+
+    let puzzle2 = match movements.iter().fold(
+        (0, 0, 0),
+        |(aim, z, y), movement| match movement {
+            ("forward", d) => (aim, z + d, y + d * aim),
+            ("down", d) => (aim + d, z, y),
+            ("up", d) => (aim - d, z, y),
+            _ => (aim, z, y),
+        },
+    ) {
+        (_, z, y) => z * y,
+    };
+
+    println!("1st puzzle: {}", puzzle1);
+    println!("2nd puzzle: {}", puzzle2);
+}
+
 fn main() {
     println!("❄️❄️❄️ Day one ❄️❄️❄️");
     solve_day1();
+
+    println!("❄️❄️❄️ Day two ❄️❄️❄️");
+    solve_day2();
 }
